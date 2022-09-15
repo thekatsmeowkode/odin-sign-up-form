6e9110d84da5d5ea81545eaa75f958867f3ec6b5
Kat M <kmunson13@gmail.com>
2022-09-15 15:09:44 -0500

    Add working javascript for password validation

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # odin-sign-up-form
+
+
+The goal for this project is to create a signup form for a fictional service as part of the Odin Project curriculum. The objective is to create a form that responds to valid and invalid inputs. I took it a bit further to learning about some principles of design and making an aesthetically pleasing page along the way.
+
+Areas of learning focus:
+
+Color wheel theory
+Free stock image resources on the web & how to properly credit to artists
+Use of pseudoclasses
+More complex CSS selector usages integrated
+How to link external custom fonts to project using Google Fonts and downloading files/generating webkit
+Be mindful of accessibility features - very interested in this topic because of relevance to jobs
+How to integrate Javascript into form validation and password matching
--- form.js
+++ form.js
@@ -3,28 +3,45 @@ const email = document.querySelector('.email');
 const password = document.querySelector('#password');
 const submitButton = document.querySelector('button');
 const confirmpassword = document.querySelector('#confirm_password');
+const validate = document.querySelector('.validate')
+const form = document.getElementById('#form')
 
-submitButton.addEventListener("click", (e) => {
-  email.classList.toggle("error", !email.checkValidity());
-});
+// submitButton.addEventListener("click", (e) => {
+//   email.classList.toggle("error", !email.checkValidity());
+// });
 
-submitButton.addEventListener("click", (e) => {
-    password.classList.toggle("error", !password.checkValidity());
-  });
+// submitButton.addEventListener("click", (e) => {
+//     password.classList.toggle("error", !password.checkValidity());
+//   });
 
 // submitButton.addEventListener("click", (e) => {
 //     confirmpassword.classList.toggle("error", !confirmpassword.checkValidity());
 //   });
 
-function validatePassword() {
-    if (password.value !== confirmpassword.value) {
-        password.setCustomValidity('Sorry, passwords do not match.  Please try a new combination.');
-        password.style.borderColor(#880808);
-        confirmpassword.style.borderColor(#880808);
-    }
-    else (password.value === confirmpassword.value)
-    {password.style.borderColor(#00FF00)
-    confirmpassword.style.borderColor(#00FF00)}
+let match=false
+
+function check() {
+if (password.value == confirmpassword.value) {
+    password.style.border = "2px solid green";
+    confirmpassword.style.border = "2px solid green";
+    validate.innerText = "";
+    match = true;
+  } 
+  else {
+    password.style.border = "2px solid red";
+    confirmpassword.style.border = "2px solid red"
+    validate.innerText = "* Passwords do not match";
+    match = false;
+  }}
+
+
+function submit_form() {
+  if (!match) {
+    validate.innerText = "Passwords must match to submit form, please try again";
+    submitButton.disabled = true
+  } else {
+    document.form['form'].submit();
+    submitButton.disabled= false
+  }
 }
 
-password.onchange = validatePassword()
